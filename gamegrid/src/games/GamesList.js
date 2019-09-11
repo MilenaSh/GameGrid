daa8d7bd40628db6c3fcf4506ff34b173be3698c
Milena Shishkova <milena@a-triks.com>
2019-09-11 20:01:59 +0300

    First view done

--- gamegrid/src/games/GamesList.js
+++ gamegrid/src/games/GamesList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import GamesData from '../data/grid-app-data.json';
+import {Row, Col, Image} from 'react-bootstrap';
+
 class GamesList extends Component {
 idWasClicked() {
     alert('Id wa clicked');
@@ -8,12 +10,28 @@ idWasClicked() {
   render() {
     return (
       <div className="App">
-      <h1>Hello there</h1>
-      {
-          GamesData.categories[0].games.map((gameDetail, index) => {
-              return <h1 onClick={this.idWasClicked}>{gameDetail.id}</h1>
-          })
-      }
+      <h1>Category Title</h1>
+  <Row>
+    <Col lg={4} className={"mainGame"}>
+    <Image src={GamesData.categories[0].games[0].thumb} fluid />
+    <div> {GamesData.categories[0].games[0].name}</div> 
+   </Col>
+    <Col lg={8} >
+        <Row>
+              {
+                  GamesData.categories[0].games.map((gameDetail, index) => {
+                    if(index !== 0) {                    
+                      return <Col lg={4} >
+                        <Image src={gameDetail.thumb} fluid />
+                       <div>{gameDetail.name}</div> 
+                      </Col>
+                    }
+                  })
+              }           
+        </Row>
+    </Col>
+  </Row>
+
       </div>
     );
   }
