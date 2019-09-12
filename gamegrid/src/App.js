6b7f89ab07cc4e1e4ea3f1dbbb02067d5492952f
Milena Shishkova <milena@a-triks.com>
2019-09-12 18:28:56 +0300

    Added routing

--- gamegrid/src/App.js
+++ gamegrid/src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import GamesList from './games/GamesList';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Link} from 'react-router-dom'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-     <GamesList/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <GamesList/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- gamegrid/src/games/GamesList.js
+++ gamegrid/src/games/GamesList.js
@@ -1,40 +1,70 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import GamesData from '../data/grid-app-data.json';
 import {Row, Col, Image} from 'react-bootstrap';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 
 class GamesList extends Component {
-idWasClicked() {
-    alert('Id wa clicked');
+
+    render() {
+        return (
+            <Router>
+                <Route path="/game/:gameDetailId" component={GameActive}/>
+                <Route exact={true} path="/" component={GameGridHome}/>
+            </Router>
+        );
+    }
 }
 
-  render() {
-    return (
-      <div className="App">
-      <h1>Category Title</h1>
-  <Row>
-    <Col lg={4} className={"mainGame"}>
-    <Image src={GamesData.categories[0].games[0].thumb} fluid />
-    <div> {GamesData.categories[0].games[0].name}</div> 
-   </Col>
-    <Col lg={8} >
+const GameGridHome = ({match}) => (
+    <div className="App">
+        <h1>Category Title</h1>
         <Row>
-              {
-                  GamesData.categories[0].games.map((gameDetail, index) => {
-                    if(index !== 0) {                    
-                      return <Col lg={4} >
-                        <Image src={gameDetail.thumb} fluid />
-                       <div>{gameDetail.name}</div> 
-                      </Col>
-                    }
-                  })
-              }           
-        </Row>
-    </Col>
-  </Row>
-
-      </div>
-    );
-  }
+            <Col lg={4} className={"mainGame"}>
+                <Link to={`/game/${GamesData.categories[0].games[0].id}`}>
+                    <div className="menu-item-wrapper">
+                        <Image src={GamesData.categories[0].games[0].thumb} fluid/>
+                        <div>{GamesData.categories[0].games[0].name}</div>
+                    </div>
+                </Link>
+            </Col>
+            <Col lg={8}>
+                <Row>
+                    {GamesData
+                        .categories[0]
+                        .games
+                        .map((gameDetail, index) => {
+                            if (index !== 0) {
+                                return <Col lg={4}>
+                                    <Link to={`/game/${gameDetail.id}`}>
+                                        <div className="menu-item-wrapper">
+                                            <Image src={gameDetail.thumb} fluid/>
+                                            <div>{gameDetail.name}</div>
+                                        </div>
+                                    </Link>
+                                </Col>
+                            }
+                        })
 }
+                </Row>
+            </Col>
+        </Row>
+    </div>
+)
+
+const GameActive = ({match}) => (
+    <div className="game-active">
+        <Row>
+            <Col lg={10}></Col>
+            <Col lg={2}>
+                <Image
+                    src={GamesData.categories[0].games[`${match.params.gameDetailId}`].thumb}
+                    fluid/>
+                <div>{GamesData.categories[0].games[`${match.params.gameDetailId}`].name}</div>
+                <div>{GamesData.categories[0].games[`${match.params.gameDetailId}`].description}</div>
+            </Col>
+        </Row>
+
+    </div>
+)
 
 export default GamesList;
